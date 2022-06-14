a219a4b103b40e5fd49c9f00449502b328de27c7
jangkyoungtae <zxs3710@naver.com>
2022-06-15 00:17:12 +0900

    GithubAction Test

--- src/API/BoaderApi.tsx
+++ src/API/BoaderApi.tsx
@@ -8,43 +8,28 @@ import { IBoaderList } from '../Types/boaderType';
 import setupInterceptorsTo from './Interceptors';
 
 const boaderApi = axios.create({
-	baseURL: 'http://3.35.233.99/api',
-	// baseURL: 'http://localhost:5008/',
+	// baseURL: 'http://13.209.99.119/api/',
+	baseURL: 'http://localhost:5008/',
 });
 const callUrl = setupInterceptorsTo(boaderApi);
-// axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-// boaderApi.interceptors.request.use(
-// 	function (config) {
-// 		if (config.headers) {
-// 			config.headers.Authorization = localStorage.getItem('Authorization') || 0 || false;
-// 		}
-// 		return config;
-// 	},
-// 	function (error) {
-// 		// 요청 오류가 있는 작업 수행
-// 		return Promise.reject(error);
-// 	}
-// );
+boaderApi.interceptors.request.use(
+	function (config) {
+		if (config.headers) {
+			config.headers.Authorization = localStorage.getItem('Authorization') || 0 || false;
+		}
+		return config;
+	},
+	function (error) {
+		// 요청 오류가 있는 작업 수행
+		return Promise.reject(error);
+	}
+);
 
-const callBoaderList = async () => {
-	const data = await axios.get(`http://3.35.233.99/api/board`);
+const callBoaderList = async ({ pageParam = 1 }) => {
+	const data = await callUrl.get(`/borders?_sort=id&_order=desc&_page=${pageParam}&_limits=6`);
 	return data;
-};
-
-const callAddBoard = async (value: FieldValues, type: number) => {
-	const forms = new FormData();
-	forms.append('file', value.image[0]);
-	forms.append('content', value.Contents);
-	console.log(forms);
-	const res = await axios.post('http://3.35.233.99/api/board', forms, {
-		headers: {
-			'X-Auth-Token':
-				'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0NDFAdDUudDYiLCJyb2xlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTY1NTIxMTM3NiwiZXhwIjoxNjU1NTcxMzc2fQ.t4RbjNWHYIyaUhcXPLBKUT_yiqB_F9qubp614G2ImzU',
-			'Content-Type': 'multipart/form-data',
-		},
-	});
-	return res;
 };
 
 // const callAddBoard = async (value: FieldValues, type: number) => {
@@ -55,15 +40,37 @@ const callAddBoard = async (value: FieldValues, type: number) => {
 // 		likes: 0,
 // 		img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5U2J4WdPI9AY_3LNPpTSDAdYgzZQDD9-dGg&usqp=CAU',
 // 	};
+// 	const forms = new FormData();
+// 	forms.append('file', value.image[0]);
+// 	forms.append('content', value.Contents);
 
-// 	const res = await callUrl.post('/borders', addDatas);
+// 	const res = await callUrl.post('/borders', forms, {
+// 		headers: {
+// 			'X-Auth-Token': process.env.ACCESS_TOKEN || false,
+// 			'Content-Type': 'multipart/form-data',
+// 		},
+// 	});
 // 	return res;
 // };
+
+const callAddBoard = async (value: FieldValues, type: number) => {
+	const addDatas = {
+		nickname: '김종국',
+		content: value.Contents,
+		type,
+		likes: 0,
+		img_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5U2J4WdPI9AY_3LNPpTSDAdYgzZQDD9-dGg&usqp=CAU',
+	};
+
+	const res = await callUrl.post('/borders', addDatas);
+	return res;
+};
 
 const callDelBoard = async (card?: IBoaderList) => {
 	const data = await callUrl.delete(`/borders/${card?.id}`);
 	return data;
 };
+
 const callModifyBoard = async (value: FieldValues, tpye: number, card?: IBoaderList) => {
 	const addDatas = {
 		nickname: card?.nickname,
@@ -101,7 +108,7 @@ export const userApi = {
 };
 
 export const boardApi = {
-	callBoaderList: () => callBoaderList(),
+	callBoaderList: (pageParam: number) => callBoaderList({ pageParam }),
 	callDelBoard: (card?: IBoaderList) => callDelBoard(card),
 	callAddBoard: (data: FieldValues, type: number) => callAddBoard(data, type),
 	callModifyBoard: (value: FieldValues, type: number, card?: IBoaderList) => callModifyBoard(value, type, card),
