5d737fe7b32092c1409de0d010e53f5620952c59
jangkyoungtae <zxs3710@naver.com>
2022-06-16 12:06:15 +0900

    api user board 분할 및 url 환경 변수 방식으로 변경

--- src/API/baseApi.tsx
+++ src/API/baseApi.tsx
@@ -3,7 +3,7 @@ import setupInterceptorsTo from './Interceptors';
 
 const boaderApi = axios.create({
 	// baseURL: 'http://codjaeho.shop/api',
-	baseURL: `http://13.209.65.162/api`,
+	baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
 });
 
 // http://34.197.218.191:8080/api/board 상훈님 url
