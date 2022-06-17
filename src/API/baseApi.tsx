869a96f6c05ac7d854802f8727f3bdac3f683245
jangkyoungtae <zxs3710@naver.com>
2022-06-17 21:00:40 +0900

    api 배포용 url 로 변경

--- src/API/baseApi.tsx
+++ src/API/baseApi.tsx
@@ -3,7 +3,7 @@ import setupInterceptorsTo from './Interceptors';
 
 const boaderApi = axios.create({
 	// baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
-	baseURL: 'http://hhmagazine.s3-website.ap-northeast-2.amazonaws.com/api',
+	baseURL: 'http://13.209.65.162/api',
 });
 
 // http://34.197.218.191:8080/api/board 상훈님 url
